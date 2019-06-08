5b0d4ad8fa90833aa0cb746720cbe1295e5c010e
Timothy Cumberland <tdkcumberland@gmail.com>
2019-06-08 10:35:56 -0400

    end of lecture

--- Part1/src/index.js
+++ Part1/src/index.js
@@ -2,11 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-const App = () => (
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10}/>
+      <Hello name={name} age={age}  />
+      <Footer />
+    </>
+  )
+}
+
+const Hello = (props) => {
+  return (
     <div>
-      <p>Hello world</p>
+      <p>
+      Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by 
+      <a href="https://github.com/tdkcumberland">tdkcumberland</a>
+    </div>
+  )
+}
   
   ReactDOM.render(<App />, document.getElementById('root'))
   
