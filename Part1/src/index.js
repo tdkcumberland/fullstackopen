84fa21892eb31dda2c384b0457a58b73d0f9c7be
Timothy Cumberland <tdkcumberland@gmail.com>
2019-08-31 21:25:07 -0400

    upto exercise 1.14*

--- Part1/src/index.js
+++ Part1/src/index.js
@@ -2,69 +2,6 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-  return (
-    <>
-      <Header course={course} />
-      <Content2 part={course} />
-      <Total exercises={course} />
-    </>
-  )
-}
-
-const Header = (props) => {
-  // console.log(props.course.name)
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Content2 = (props) => {
-  const testPart = [];
-  for (var i = 0; i < props.part.parts.length; i+=1){
-    // console.log(i)
-    // console.log(props.part.parts[i])
-    console.log(props.part.parts[i].name.toString())
-    testPart.push(<p>{props.part.parts[i].name} {props.part.parts[i].exercises}</p> )
-  }
-
-  const listItems = props.part.parts.map(both =>
-  <p key={both.name.toString()}>{both.name} {both.exercises}</p>
-  );
- 
-  return (
-    listItems
-  )
-}
-
-const Total = (props) => {
-  var total = 0;
-  for (var i = 0; i < props.exercises.parts.length; i+=1) {
-    total += (props.exercises.parts[i].exercises);
-  }
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
-const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({onClick, text}) => (
   <button onClick={onClick}>
@@ -72,96 +9,109 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
-
-
-const History = (props) => {
-  if (props.allClicks.length === 0){
+const Statistic = ({text, value, clickCount}) => {
+  if (clickCount === 0){
     return (
+      <tr>
+      <td>{text}</td>
+      <td>no feed back gathered</td>
+    </tr>
+      
+    )
+  }
+    return (
+      <>
+        <tr>
+          <td>{text}</td>
+          <td>{String(value)}</td>
+        </tr>
+      </>
+    )
+}
+
+const StatisticTable = ({good, bad, neutral, clickCount}) => {
+  if (clickCount === 0){
+    return(
+      <>
       <div>
-        the app is used by pressing the buttons
+        <b><i>no feedback has been gathered</i></b>
       </div>
+      </>
     )
   }
   return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
+    <>
+      <table>
+        <tbody>
+          <Statistic text='good' value={good} clickCount={clickCount}/>
+          <Statistic text='neutral' value={neutral} clickCount={clickCount}/>
+          <Statistic text='bad' value={bad} clickCount={clickCount}/>
+          <Statistic text='all' value={bad + good + neutral} clickCount={clickCount}/>
+          <Statistic text='average' value={(bad*-1 + good*1 + neutral*0)/(bad + good + neutral)} clickCount={clickCount}/>
+          <Statistic text='positive' value={good/(bad + good + neutral) + '%'} clickCount={clickCount}/>
+        </tbody>
+      </table>
+    </>
   )
 }
 
-const App1 = (props) => {
-  const [ counter, setCounter ] = useState(0)
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const setToValue = (value) => setCounter(value)
-  const setLeftToValue = (value) => setLeft(value)
-  const setRightToValue = (value) => setRight(value)
-  const [allClicks, setAll] = useState([])
-  const [clicks, setClicks] = useState(
-    {left: 0, right: 0}  
-    )
-  
-    const handleLeftClick = () => {
-      // const newClicks ={
-      //   ...clicks,
-      //   left: clicks.left + 1
-      // }
-      // setClicks(newClicks)
-      setAll(allClicks.concat('L'))
-      setLeft(left + 1)
-    }
-    
-    const handleRightClick = () => {
-      // const newClicks = {
-      //   ...clicks,
-      //   right: clicks.right + 1
-      // }
-      // setClicks(newClicks)
-      setAll(allClicks.concat('R'))
-      setRight(right + 1)
-    }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const App1 = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [clickCount, setCount] = useState(0)
+  const [selected, setSelected] = useState(0)
+
+  const onGoodClick = () => {setGood(good + 1); setCount(clickCount + 1)}
+  const onNeutralClick = () => {setNeutral(neutral + 1); setCount(clickCount + 1)}
+  const onBadClick = () => {setBad(bad+ 1); setCount(clickCount + 1)}
+  // const onReset = () => {
+  //   setGood(0)
+  //   setBad(0)
+  //   setNeutral(0)
+  // }
+  const onSelect = () => {
+    setSelected(Math.floor((Math.random() * 5) + 0))
+  }
 
   return (
     <>
+    <h1>quotes</h1>
     <div>
-      <Display counter={counter}/>
-      <Button onClick={() => setToValue(counter + 1)}
-        text = 'plus'
-      />
-      <Button onClick={() => setToValue(counter - 1)}
-        text = 'minus'
-      />
-      <Button onClick={() => setToValue(0)}
-        text = 'zero'
-      />
-      <Button onClick={() => setToValue(counter * 3)}
-        text = 'x2'
-      />
-      <Button onClick={()=>setToValue(counter / 2)} 
-        text = '/2'
-      />
+      {props.anecdotes[selected]} 
     </div>
+    <Button onClick={onSelect} text='vote'/>
+    <Button onClick={onSelect} text='next anecdote'/>
     
+    <h1>give feedback</h1>
     <div>
-      <Display counter={left}/>
-      <Display counter={right}/>
-      <History allClicks={allClicks}/>
-      <Button onClick={handleLeftClick} text = 'left'/>
-      <Button onClick={handleRightClick} text = 'right'/>
+      <Button onClick={onGoodClick} text='good'/>
+      <Button onClick={onNeutralClick} text='neutral'/>
+      <Button onClick={onBadClick} text='bad'/>
+      {/* <Button onClick={onReset} text='reset'/> */}
     </div>
+    <h1>statistic</h1>
+    <StatisticTable good={good} bad={bad} neutral={neutral} clickCount={clickCount} />
     </>
   )
 }
 
 ReactDOM.render(
-  <App1 />, 
-  document.getElementById('root')
+    <App1 anecdotes={anecdotes}/>, 
+    document.getElementById('root')
 )
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
---
+++ Part1/src/indexOLD.js
@@ -0,0 +1,165 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header course={course} />
+      <Content2 part={course} />
+      <Total exercises={course} />
+    </>
+  )
+}
+
+const Header = (props) => {
+  // console.log(props.course.name)
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content2 = (props) => {
+  const testPart = [];
+  for (var i = 0; i < props.part.parts.length; i+=1){
+    // console.log(i)
+    // console.log(props.part.parts[i])
+    console.log(props.part.parts[i].name.toString())
+    testPart.push(<p>{props.part.parts[i].name} {props.part.parts[i].exercises}</p> )
+  }
+
+  const listItems = props.part.parts.map(both =>
+  <p key={both.name.toString()}>{both.name} {both.exercises}</p>
+  );
+ 
+  return (
+    listItems
+  )
+}
+
+const Total = (props) => {
+  var total = 0;
+  for (var i = 0; i < props.exercises.parts.length; i+=1) {
+    total += (props.exercises.parts[i].exercises);
+  }
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const App1 = (props) => {
+  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const setToValue = (value) => setCounter(value)
+  const setLeftToValue = (value) => setLeft(value)
+  const setRightToValue = (value) => setRight(value)
+  const [allClicks, setAll] = useState([])
+  const [clicks, setClicks] = useState(
+    {left: 0, right: 0}  
+    )
+  
+    const handleLeftClick = () => {
+      // const newClicks ={
+      //   ...clicks,
+      //   left: clicks.left + 1
+      // }
+      // setClicks(newClicks)
+      setAll(allClicks.concat('L'))
+      setLeft(left + 1)
+    }
+    
+    const handleRightClick = () => {
+      // const newClicks = {
+      //   ...clicks,
+      //   right: clicks.right + 1
+      // }
+      // setClicks(newClicks)
+      setAll(allClicks.concat('R'))
+      setRight(right + 1)
+    }
+
+
+  return (
+    <>
+    <div>
+      <Display counter={counter}/>
+      <Button onClick={() => setToValue(counter + 1)}
+        text = 'plus'
+      />
+      <Button onClick={() => setToValue(counter - 1)}
+        text = 'minus'
+      />
+      <Button onClick={() => setToValue(0)}
+        text = 'zero'
+      />
+      <Button onClick={() => setToValue(counter * 3)}
+        text = 'x2'
+      />
+      <Button onClick={()=>setToValue(counter / 2)} 
+        text = '/2'
+      />
+    </div>
+    
+    <div>
+      <Display counter={left}/>
+      <Display counter={right}/>
+      <History allClicks={allClicks}/>
+      <Button onClick={handleLeftClick} text = 'left'/>
+      <Button onClick={handleRightClick} text = 'right'/>
+    </div>
+    </>
+  )
+}
+
+ReactDOM.render(
+  <App1 />, 
+  document.getElementById('root')
+)
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
