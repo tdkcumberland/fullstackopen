c14ba427d2b3257b225f21bfb4b44f82a6676e61
Timothy Cumberland <tdkcumberland@gmail.com>
2019-09-25 20:54:00 -0400

    part 2-c done

--- Part2/src/ShowDetail.js
+++ Part2/src/ShowDetail.js
@@ -1,10 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import CountryLang from './CountryLang'
 import axios from 'axios'
 
 const ShowDetail = ({languages, capital,population, flag}) => {
     // console.log(props)
-    const ACCESS_KEY = 'e94f8f549694a0c8a49ca8cd9a210790'
     console.log('SHOWDETAIL')
     const languageRow = () => languages.map( eachLang =>
         <CountryLang
@@ -12,23 +11,17 @@ const ShowDetail = ({languages, capital,population, flag}) => {
             key = {eachLang.name}
         />
     )
-    const CancelToken = axios.CancelToken;
-    const source = CancelToken.source();
 
-    const [weather, setWeather] = useState('')
-    const weatherData = () =>{
+    const [chuckQuote, setChuckQuote] = useState('')
+    useEffect(()=>{
+        console.log("getting a Chuck Norris' quote...")
         axios
-        .get(`http://api.weatherstack.com/current?access_key=45823a7f39174c1490b63c6bc6bcc0f0&query=${capital}`, { cancelToken: source.token })
-        .then(response =>{
-            setWeather(response.data.current)
-            console.log(response.data.current)
-                })
-
-        weatherData();
-        return () => {
-            source.cancel();
-        };
-    }
+            .get('https://api.chucknorris.io/jokes/random')
+            .then( response => {
+                console.log('got one...')
+                setChuckQuote(response.data.value)
+            })
+    }, [])
 
     return (
         <>
@@ -43,8 +36,11 @@ const ShowDetail = ({languages, capital,population, flag}) => {
                 {languageRow()}
             </div>
             <img src={flag.toString()} style={{width:300, height:175}} alt='flag'></img>
+            <h5>
+                Random Chuck Norris' Quote :)
+            </h5>
             <div>
-                {weatherData()}
+                {chuckQuote}
             </div>
         </>
     )
